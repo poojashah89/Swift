32841729d72c2d752cde3b8b09480e64ad50e9d7
Pooja <shah.pooja89@gmail.com>
2017-12-20 15:17:34 -0800

    final

--- Skin Rash Diagnosis/Pods/Target Support Files/Pods-AppSquadJavascriptWikitudeSwiftTests/Pods-AppSquadJavascriptWikitudeSwiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AppSquadJavascriptWikitudeSwiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_AppSquadJavascriptWikitudeSwiftTests
-@end
