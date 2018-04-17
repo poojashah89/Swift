dd4585e661acb499544c5b72884af18e54d47782
Pooja <pooja.k.shah@ibm.com>
2018-04-17 13:48:06 -0700

    chatbot

--- tensorflow/Chatbot/client.py
+++ tensorflow/Chatbot/client.py
@@ -1,11 +1,8 @@
 
 import Pyro4
+import sys
+
+question = sys.argv[1]
 get = Pyro4.Proxy("PYRONAME:example.bot")
-get.get_response('What hours are you open?')
-
-import Pyro4
-
-name = input("What hours are you open?").strip()
-
-get = Pyro4.Proxy("PYRONAME:example.bot")    # use name server object lookup uri shortcut
-print(get.get_response(name))
+name = get.get_response(question)
+print(name)
