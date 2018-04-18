6c609cd01cf42c8702c1652bd3fb034bb286a106
Pooja <pooja.k.shah@ibm.com>
2018-04-17 18:04:59 -0700

    uichanges

---
+++ DermaCare/DermaCare/Controller/HistoryController.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryController.swift
+//  DermaCare
+//
+//  Created by Pooj on 4/17/18.
+//  Copyright © 2018 Pooja. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryController: UIViewController {
+}
