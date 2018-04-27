6753a84116273af95251b28095ad7aa403bda9f3
Pooja <pooja.k.shah@ibm.com>
2018-04-26 21:49:49 -0700

    pharmacy

---
+++ DermaCare/DermaCare/Controller/PharmacyController.swift
@@ -0,0 +1,16 @@
+//
+//  PharmacyController.swift
+//  DermaCare
+//
+//  Created by Pooj on 4/26/18.
+//  Copyright © 2018 Pooja. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class PharmacyController: UIViewController
+{
+    
+}
