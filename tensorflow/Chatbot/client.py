07febc9bb08ceeaad92784f83433f0718c967fe3
Ubuntu <ubuntu@ip-172-31-31-254.us-west-2.compute.internal>
2018-05-07 18:44:29 +0000

    chatport

--- tensorflow/Chatbot/client.py
+++ tensorflow/Chatbot/client.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route("/chat")
 def chat():
     get = Pyro4.Proxy("PYRONAME:example.bot")
+    print("hi")
     question = request.args.get('chat')
     name = get.get_response(question)
     return name
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5000)
+
--- tensorflow/Chatbot/testflask.py
+++ tensorflow/Chatbot/testflask.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 def hello_world():
 	print('hi')
 	return 'hi pooja'
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5000)
