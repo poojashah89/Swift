1222fb81b414923b42001f293a3efe574592f063
Ubuntu <ubuntu@ip-172-31-31-254.us-west-2.compute.internal>
2018-05-07 22:34:38 +0000

    chatbotfix

--- tensorflow/Chatbot/client.py
+++ tensorflow/Chatbot/client.py
@@ -1,16 +1,19 @@
-from flask import Flask, request 
+from flask import Flask, request, jsonify 
 import Pyro4
 import sys
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/chat")
 def chat():
     get = Pyro4.Proxy("PYRONAME:example.bot")
-    print("hi")
+
     question = request.args.get('chat')
     name = get.get_response(question)
-    return name
+    dtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(dtime)
+    return jsonify(message = name, time=str(dtime))
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=5000)
